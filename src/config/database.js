6016ad33bf9fe6a3e66d9360d02906efd8a7500d
Weslley Borges <weslleynfs@gmail.com>
2019-11-19 18:24:58 -0400

    Remoção do package-lock, inclusão do Procfile e adaptação nas configurações para acesso ao banco de dados

--- src/config/database.js
+++ src/config/database.js
@@ -1,4 +1,8 @@
 const mongoose = require('mongoose')
 mongoose.Promise = global.Promise
 
-module.exports = mongoose.connect('mongodb://localhost:27017/todoapp')
+const LOCALHOST_MONGO_URL_CONNECTION = 'mongodb://localhost:27017/todoapp'
+const MONGOLAB_ENV_VAR = process.env.MONGOLAB_URI
+const URL_CONNECTION = MONGOLAB_ENV_VAR ? MONGOLAB_ENV_VAR : LOCALHOST_MONGO_URL_CONNECTION
+
+module.exports = mongoose.connect(URL_CONNECTION, { useMongoClient: true })
